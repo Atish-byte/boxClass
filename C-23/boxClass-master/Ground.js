6d6879cd6023d4f7952ac77ef17538d3672dd5a4
Atish-byte <atishmalharpatil@gmail.com>
2020-05-13 14:20:46 +0530

    Add files via upload

---
+++ C-23/boxClass-master/Ground.js
@@ -0,0 +1,18 @@
+class Ground {
+constructor(x,y,width,heigth){
+    var option = {
+        isStatic: true
+    }
+    this.body= Bodies.rectangle(x,y,width,heigth,option);
+    this.width=width;
+    this.heigth=heigth;
+
+    World.add(world,this.body)
+}
+display(){
+    var pos = this.body.position;
+    rectMode(CENTER);
+    fill(176)
+    rect(pos.x,pos.y,this.width,this.heigth)
+}
+}
